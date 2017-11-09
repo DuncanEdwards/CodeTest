19f8bbdb576e2d2881663b1dfec17fe41361f553
Duncan Edwards <dun_edwards@yahoo.com>
2017-11-09 22:39:12 +0000

    MVC Tests done

---
+++ MVCTests/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using AttributeRouting;
+using AttributeRouting.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTests.Controllers
+{
+
+    [RoutePrefix("invitationdigital")]
+    public class InvitationController : Controller
+    {
+        [Route("tests/{id}")]
+        public ActionResult Tests(string id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+
+
+    }
+}
