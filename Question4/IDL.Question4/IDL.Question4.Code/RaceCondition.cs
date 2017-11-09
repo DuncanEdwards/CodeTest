03f48d9b9d4c8c448b18bb7ac342283c4d28fe1c
Duncan Edwards <dun_edwards@yahoo.com>
2017-11-09 23:19:24 +0000

    Next cut

---
+++ Question4/IDL.Question4/IDL.Question4.Code/RaceCondition.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace TechnicalTests
+{
+    public class RaceCondition
+    {
+        private static readonly Dictionary<string, object> _lockDictionary = new Dictionary<string,object>();
+
+        private readonly ISessionManager _sessionManager;
+
+        public RaceCondition(ISessionManager sessionManager)
+        {
+            _sessionManager = sessionManager;
+        }
+
+        public void UpdateSession(string sessionKey)
+        {
+            // A simple lock won't do, lets use a simple lock dictionary
+            if (!_lockDictionary.ContainsKey(sessionKey))
+            {
+                _lockDictionary.Add(sessionKey, new object());
+            }
+
+            // Then we have key dependant locking, happy days
+            // Unless, this is completely wrong, then, unhappy days
+            lock (_lockDictionary[sessionKey])
+            {
+                var session = _sessionManager.Get(sessionKey);
+                session.Count += 1;
+                _sessionManager.Set(session);
+            }
+
+
+        }
+    }
+}
---
+++ Question4/IDL.Question4/IDL.Question4.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IDL.Question4.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IDL.Question4.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8a88a029-51c0-4b3c-a349-4b9d981e980e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
